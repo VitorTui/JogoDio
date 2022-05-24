3ce6327e1b0482ad5c5d6caac48d1501769c9367
vitor <vitor.petro2@gmail.com>
2022-05-24 17:47:20 -0300

    Fim

--- src/Entities/BlackWizard.cs
+++ src/Entities/BlackWizard.cs
@@ -5,8 +5,29 @@ using System.Threading.Tasks;
 
 namespace Vitor_Silva.src.Entities
 {
-    public class BlackWizard
+ public class BlackWizard : Hero
     {
-        
+        public BlackWizard(string Name, int Level, string HeroType)
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+        }
+
+        public override string Attack()
+        {
+            return this.Name + " Lançou o feitiço maléficos das trevas";
+        }
+
+        public string Attack(int Bonus){
+
+            if (Bonus > 12){
+                return this.Name + " Lançou uma magia Boladona com forças de todos os magos do universo " + Bonus;
+               } else{ 
+                    return this.Name + " Lançou um truque fajuto de quinta " + Bonus;
+                }
+            }
+           
     }
+
 }
--- src/Program.cs
+++ src/Program.cs
@@ -11,10 +11,12 @@ namespace Vitor_Silva
             Knight arus = new  Knight("Arus", 23, "Knight");
             Wizard wizard = new Wizard(" Jennica", 23, " White Wizard");
             Ninja  wedge = new Ninja(" Wedge", 42, "Ninja");
+            BlackWizard topapa = new BlackWizard(" Topapa", 42, "BlackWizard");
             
-            Console.WriteLine(wizard.Attack(1));
+            Console.WriteLine(wizard.Attack(13));
             Console.WriteLine(wizard.Attack(7));
             Console.WriteLine(wedge.Attack(7));
+            Console.WriteLine(topapa.Attack(77));
         }
     }
 }
