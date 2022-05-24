31334db1247109867c3fdbe179d77ba81557e000
vitor <vitor.petro2@gmail.com>
2022-05-24 11:04:17 -0300

    Organização de Pastas

---
+++ src/Entities/Arus.cs
@@ -0,0 +1,15 @@
+namespace Entities
+{
+    public class Arus
+    {
+        public Arus(string Name, int Level, string HeroType)
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+        }
+        public string Name;
+        public int Level;
+        public string HeroType;
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Entities;
+
+namespace Vitor_Silva
+{
+
+  class Program
+    {
+        static void Main(string[] args)
+        {
+            Arus hero = new Arus("Arus", 23, "knight");
+           
+            Console.Write("");
+        }
+    }
+}
